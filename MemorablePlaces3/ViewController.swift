f6b4fa6b98ac6e97fd9c34e89f02cfa1bc4e14b0
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-07 13:37:26 +0000

    Initial Commit

---
+++ MemorablePlaces3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemorablePlaces3
+//
+//  Created by Julian Nicholls on 07/01/2016.
+//  Copyright © 2016 Really Big Shoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
